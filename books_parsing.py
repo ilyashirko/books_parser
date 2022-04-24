79995cff304ef50aadb1e4416913b66103076d66
ilyashirko <82837921+ilyashirko@users.noreply.github.com>
2022-04-24 13:02:53 +0300

    add covers download

--- books_parsing.py
+++ books_parsing.py
@@ -2,11 +2,17 @@ import requests
 import os
 from bs4 import BeautifulSoup as bs
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, unquote, urlsplit
+
+
+BASE_TULULU_URL = 'https://tululu.org/'
 
 BOOK_FOLDER = 'Books'
 
+COVER_FOLDER = 'Covers'
 
-def get_valid_response(book_id):
+
+def get_valid_book(book_id):
     url = f'https://tululu.org/txt.php?id={book_id}'
     response = requests.get(url)
     response.raise_for_status()
@@ -20,12 +26,17 @@ def get_book_info(book_id):
     response = requests.get(url)
     response.raise_for_status()
     page_source = bs(response.text, 'html.parser')
+
     book_info = page_source.find('td', class_='ow_px_td')
     book_name, book_author = book_info.find('h1').text.split('::')
-    return book_name.strip(), book_author.strip()
+
+    book_cover_path = page_source.find('div', class_='bookimage').findChild('img').get('src')
+    book_cover_url = urljoin(BASE_TULULU_URL, book_cover_path)
+
+    return book_name.strip(), book_author.strip(), book_cover_url
 
 
-def save_book(response, book_id, book_name):
+def download_book(response, book_id, book_name):
     correct_book_name = sanitize_filename(book_name)
     full_path = os.path.join(
         BOOK_FOLDER,
@@ -35,14 +46,29 @@ def save_book(response, book_id, book_name):
         new_book.write(response.content)
 
 
+def download_cover(cover_url):
+    _, photo_name = os.path.split(unquote(urlsplit(cover_url).path))
+    full_path = os.path.join(COVER_FOLDER, photo_name)
+    
+    if not os.path.exists(full_path):
+        response = requests.get(cover_url)
+        response.raise_for_status()
+        with open(full_path, 'wb') as photo:
+            photo.write(response.content)
+
+
 if __name__ == '__main__':
     os.makedirs(BOOK_FOLDER, exist_ok=True)
+    os.makedirs(COVER_FOLDER, exist_ok=True)
     for book_id in range(1, 11):
         try:
-            response = get_valid_response(book_id)
+            response = get_valid_book(book_id)
             if response.ok:
-                book_name, book_author = get_book_info(book_id)
-                save_book(response, book_id, book_name)
+                book_name, book_author, book_cover_url = get_book_info(book_id)
+
+                download_book(response, book_id, book_name)
+                download_cover(book_cover_url)
+
                 print(f'Book [{book_id}]: DOWNLOADED.')
                 
         except requests.HTTPError:
