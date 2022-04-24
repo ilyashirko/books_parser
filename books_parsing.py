591396574f43463e5b9d6fd737e6e0b05951fc08
ilyashirko <82837921+ilyashirko@users.noreply.github.com>
2022-04-24 10:52:24 +0300

    exceptions handled

--- books_parsing.py
+++ books_parsing.py
@@ -1,18 +1,37 @@
-from urllib import response
 import requests
 import os
 
 BOOK_FOLDER = 'Books'
-def save_file(file_name, content):
-    with open(file_name, 'wb') as new_file:
-        new_file.write(content)
 
+class AlreadyDownloaded(Exception):
+    pass
+
+
+def book_request(link):
+    response = requests.get(book_link)
+    response.raise_for_status()
+    if response.url != link:
+        raise requests.HTTPError
+    if not os.path.exists(f'{BOOK_FOLDER}/{id}.txt'):
+        with open(f'{BOOK_FOLDER}/{id}.txt', 'wb') as new_book:
+            new_book.write(response.content)
+    else:
+        raise AlreadyDownloaded
 
 
 if __name__ == '__main__':
     os.makedirs(BOOK_FOLDER, exist_ok=True)
     for id in range(1, 11):
-        response = requests.get(f'https://tululu.org/txt.php?id={id}')
-        if response.ok and not os.path.exists(f'{BOOK_FOLDER}/{id}.txt'):
-            save_file(f'{BOOK_FOLDER}/{id}.txt', response.content)
+        try:
+            book_link = f'https://tululu.org/txt.php?id={id}'
+            book_request(book_link)
+            print(f'Book [{id}]: DOWNLOADED.')
+        except requests.HTTPError:
+            print(f'Book [{id}]: NOT FOUND.')
+        except AlreadyDownloaded:
+            print(f'Book [{id}]: ALREADY DOWNLOADED.')
+        except requests.exceptions.RequestException:
+            print(f'Book [{id}]: BAD REQUEST.')
+                
+        
             
